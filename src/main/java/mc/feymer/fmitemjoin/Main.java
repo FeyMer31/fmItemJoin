53b960a37546f648f000deb6197ce5bf6acc21ee
Михаил Павлов <68995871+MishaNeYT@users.noreply.github.com>
2023-01-13 18:30:25 +0900

    click action

--- src/main/java/mc/feymer/fmitemjoin/Main.java
+++ src/main/java/mc/feymer/fmitemjoin/Main.java
@@ -4,6 +4,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
@@ -23,9 +24,15 @@ public final class Main extends JavaPlugin implements Listener {
 
     @EventHandler
     public void onJoin(@NotNull PlayerJoinEvent e) {
-
         Player player = e.getPlayer();
         player.getInventory().setItem(0, HeadUtils.getSkull(getConfig().getString("item-join.skull")));
+    }
+
+    @EventHandler
+    public void onAction(@NotNull PlayerInteractEvent e) {
+        if (e.getItem() != null && e.getItem().getItemMeta() != null && e.getItem().getItemMeta().getDisplayName().equals(getConfig().getString("item-join.name"))) {
+            e.getPlayer().performCommand(getConfig().getString("item-join.action"));
+        }
     }
 
     @Override
